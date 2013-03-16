89641c2d230263414f4e64d29bcbd99eec936f90
Adam Barta <adam@ska.ac.za>
2013-03-16 14:22:12 +0200

    upload

--- autoenc.sh
+++ autoenc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-#set -ex
+set -ex
 
-WATCHDIR=/srv/beats/chunes
-OUTPUT=/srv/beats/chunes/encoded
-ORIG=/srv/beats/chunes/originals
+WATCHDIR=/srv/beats/chunes/upload
+OUTPUT=/srv/beats/chunes/encode
+ORIG=/srv/beats/chunes/original
 BPS=160
 BPSK=160k
 
@@ -12,9 +12,9 @@ inotifywait -e moved_to -qm --format "%f" $WATCHDIR | while read f; do
 
   FNAME=$(echo "$f" | tr ' ' '_' | tr -d '[{}(),\!]' | tr -d "\'" | tr '[A-Z]' '[a-z]' | sed 's/_-_/_/g')
   
-  FNAME=${FNAME%%.mp3}
-  MP3=$FNAME.mp3
-  OGG=$FNAME.ogg
+  #FNAME=${FNAME%%.mp3}
+  MP3=$FNAME
+#  OGG=$FNAME.ogg
 
   if [[ ! -f $OUTPUT/"$MP3" ]] 
   then
@@ -22,12 +22,12 @@ inotifywait -e moved_to -qm --format "%f" $WATCHDIR | while read f; do
     (lame --mp3input --preset cbr $BPS -s 44.1 --bitwidth 16 $WATCHDIR/"$f" $OUTPUT/"$MP3")
   fi
  
-  if [[ ! -f $OUTPUT/"$OGG" ]]
-  then
-    echo -e "ogg: "$OGG
-    (avconv -i $WATCHDIR/"$f"  -map 0:a -c:a vorbis -q:a 7 -minrate $BPSK -maxrate $BPSK -b:a $BPSK -strict experimental $OUTPUT/"$OGG")
-    #(madplay -b 16 -R 44100 -S -o raw:- $WATCHDIR/"$f" | oggenc -b $BPS -r -B 16 -C 2 -R 44100 --raw-endianness 0 - -o $OUTPUT/"$OGG")
-  fi
+#  if [[ ! -f $OUTPUT/"$OGG" ]]
+#  then
+#    echo -e "ogg: "$OGG
+#    (avconv -i $WATCHDIR/"$f"  -map 0:a -c:a vorbis -q:a 7 -minrate $BPSK -maxrate $BPSK -b:a $BPSK -strict experimental $OUTPUT/"$OGG")
+#    #(madplay -b 16 -R 44100 -S -o raw:- $WATCHDIR/"$f" | oggenc -b $BPS -r -B 16 -C 2 -R 44100 --raw-endianness 0 - -o $OUTPUT/"$OGG")
+#  fi
 
   mv -v $WATCHDIR/"$f" $ORIG/ 
 
--- pickrnd.sh
+++ pickrnd.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-trap "echo chaning track; ps uax | grep cat $DIR" SIGHUP
 
-set -x
+#set -x
 
 if [[ $# -lt 1 ]];
 then
@@ -32,21 +31,39 @@ do
     N=$(( $(cat /dev/urandom | od -N3 -An -i) % $SIZE ))
   done
 
-  echo $(date +%F-%T)" ${LIST[$N]}" >> $DIR/../track
+  echo $(date +%F-%T)" $(basename ${LIST[$N]} .mp3)" >> $DIR/../track
 
 #  sleep 1
 
   #(cat "$DIR/${LIST[$N]}")
   MP3=${LIST[$N]}
-  OGG=${MP3%%.mp3}.ogg
+# OGG=${MP3%%.mp3}.ogg
+# coproc PROC_OGG ( cat $OGG > /srv/ogg )
+  coproc PROC_MP3 ( cat $MP3 > /srv/mp3 )
 
+  PIDS="$PROC_MP3_PID" #$PROC_OGG_PID"
+
+  #trap "echo changing track; kill ${PIDS[0]}; kill ${PIDS[1]}" SIGHUP
+  trap "echo changing track; kill ${PIDS[0]}" SIGHUP
+
+  echo "PIDs: "$PIDS" TRAc: "$MP3
+
+  for PID in $PIDS
+    do
+      #echo $PID
+      wait $PID
+    done
+
+  #sleep 5
+
+  
   #(~/live_audio_streaming/fan "cat '$MP3' > /srv/mp3" "cat '$OGG' > /srv/ogg")
   #coproc cat '$MP3' > /srv/mp3
 
   #coproc cat '$OGG' > /srv/ogg
 
-  (cat $MP3 > /srv/mp3) &
-  (cat $OGG > /srv/ogg)
+#  (cat $MP3 > /srv/mp3) &
+#  (cat $OGG > /srv/ogg)
 
 #  echo -e $MP3"\n"$OGG
 #notes (mkfifo /srv/beats/{mp3,ogg}) (while true; do cat /srv/beats/mp3) | streamer
